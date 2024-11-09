460e6105813936036b6e3d049770ee07e20e7d11
karanwaghh <karanwagh7860@gmail.com>
2024-11-09 17:24:27 +0530

    App Start code

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'travel',
+  webDir: 'www'
+};
+
+export default config;
